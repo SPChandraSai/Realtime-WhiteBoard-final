fc75f94e7d56fef0d05fd5d4fc045e475a5b9327
SPChandraSai <spchandrasai820@gmail.com>
2024-07-29 15:54:22 +0530

    Render deployed link

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://realtime-whiteboard-oo9e.onrender.com");
+const socket = io.connect("https://realtime-whiteboard-9kpz.onrender.com");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
